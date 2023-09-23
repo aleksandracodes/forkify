dfb2e6eb9962e3fa83f4bfb485f9b35599531f5f
Aleksandra <a.haniok@gmail.com>
2023-09-23 09:53:38 +0100

    Implement Search results and display the results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -23,6 +28,9 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    // Display spinner
+    resultsView.renderSpinner();
+
     // Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -32,6 +40,7 @@ const controlSearchResults = async function () {
 
     // Render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
