29f2f0b8f8e9aba3194cb71b48ed2f029f4163f5
Aleksandra <a.haniok@gmail.com>
2023-09-30 10:58:44 +0100

    Update html

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'f3d2378b-4d6e-4b96-b6a7-07c5c9f3b0a7';
+export const KEY = '4aeeaea5-6151-4bfb-98ef-7a700ee4a1fb';
 export const MODAL_CLOSE_SEC = 2.5;
